4f1907410d9b4b14df1e4ce86b7b51d3111b06f1
Wouter Deconinck <wdconinc@gmail.com>
2026-04-19 17:48:38 -0500

    fix: remove single quotes from error string to avoid script mangling

--- src/AsyncAdePTTransport.cc
+++ src/AsyncAdePTTransport.cc
@@ -140,8 +140,8 @@ bool AsyncAdePTTransport::InitializeGeometry(const vecgeom::cxx::VPlacedVolume *
         const unsigned int id  = lv->id();
         const bool inGPURegion = (id < numVolumes) && (auxData[id].fGPUregionId >= 0);
         if (inGPURegion) {
-          throw std::runtime_error(std::string("AsyncAdePTTransport::InitializeGeometry: volume '") +
-                                   lv->GetName() + "' is inside a GPU region but its shape cannot be "
+          throw std::runtime_error(std::string("AsyncAdePTTransport::InitializeGeometry: volume ") +
+                                   lv->GetName() + " is inside a GPU region but its shape cannot be "
                                    "serialized to the GPU. This configuration is not supported.");
         }
         vecgeom::Vector3D<vecgeom::Precision> aMin, aMax;
